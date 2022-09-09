f9fa4d7564c06dd5632bd4e47d955d8be6787eef
Fc0S4b <fcoj.sabugo@gmail.com>
2022-09-09 19:53:23 -0400

    API Limiter

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,16 @@ const router = express.Router();
 const authenticateUser = require('../middleware/authentication');
 const testUser = require('../middleware/testUser');
 const { register, login, updateUser } = require('../controllers/auth');
+const rateLimiter = require('express-rate-limit');
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: {
+    msg: 'To many requests from this IP, please try again after 15 minutes',
+  },
+});
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', apiLimiter, register);
+router.post('/login', apiLimiter, login);
 router.patch('/updateUser', authenticateUser, testUser, updateUser);
 module.exports = router;
